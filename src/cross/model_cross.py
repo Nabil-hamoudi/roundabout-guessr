ed83aac531d9beaa0091381edf92a13358152a5b
ADEpigone <alexandre.ducr@gmail.com>
2026-01-29 16:22:15 +0100

    Mise à jour modèle avec un peu de tuning

--- src/cross/model_cross.py
+++ src/cross/model_cross.py
@@ -4,29 +4,27 @@ import torch.nn.functional as F
 import rff
 import copy
 import numpy as np
-
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 LAT_MIN, LAT_MAX = 48.77, 48.97
 LON_MIN, LON_MAX = 2.22, 2.47
 
 def normalize(L):
     """
     Normalise les coordonnées (Lat, Lon) pour qu'elles tiennent
-    dans l'intervalle [-1, 1] en se focalisant sur la France.
+    dans l'intervalle [-1, 1] en se focalisant sur les bornes définies.
     """
-    # Bornes approximatives de la France (Corse incluse avec marge)
     
     latitude = L[:, 0]
     longitude = L[:, 1]
     
-    # 1. Normalisation Min-Max vers [-1, 1]
-    # Formule : 2 * (x - min) / (max - min) - 1
     y = 2 * (latitude - LAT_MIN) / (LAT_MAX - LAT_MIN) - 1
     x = 2 * (longitude - LON_MIN) / (LON_MAX - LON_MIN) - 1
     
-    # 2. Correction du ratio d'aspect (Important pour la distance physique !)
-    # En France, 1 degré de longitude est plus court qu'1 degré de latitude
-    # cos(46.5°) est environ 0.68. On écrase un peu les X pour garder la cohérence spatiale.
-    x = x * 0.68 
+    #calcul du ratio de déformation
+    lat_mean_rad = np.radians((LAT_MIN + LAT_MAX) / 2)
+    aspect_ratio = np.cos(lat_mean_rad)
+    
+    x = x * aspect_ratio
     
     return torch.stack((x, y), dim=1)
 
@@ -67,11 +65,15 @@ class ResBlock(nn.Module):
 class LocationEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-    
-        self.sigmas = [1.0, 2.0, 4.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0]
+        self.hidden_dim = 1024
+
+        #pour les bons signaux on utilise généralement
+        #largeur_zone/(2sigma) pour donner une idée de précision en tous les cmb de mètres
+        #sigma = 64 -> ~~1km de précision
+        self.sigmas = [1, 2, 4, 8, 16, 32, 64, 128]
         
         encoded_size = 256
-        hidden_dim = 1024 
+        hidden_dim = self.hidden_dim 
         
         self.rff_layers = nn.ModuleList([
             rff.layers.GaussianEncoding(sigma=s, input_size=2, encoded_size=encoded_size)
@@ -104,7 +106,7 @@ class FeatureAdapter(nn.Module):
         self.down = nn.Linear(in_dim, bottleneck)
         self.act = nn.GELU()
         self.up = nn.Linear(bottleneck, in_dim)
-        self.scale = nn.Parameter(torch.ones(1) * 1)
+        self.scale = nn.Parameter(torch.ones(1) * 0.1)
     
     def forward(self, x):
         # Residual: x + scale * adapter(x)
@@ -118,7 +120,7 @@ class ImageEncoder(nn.Module):
         
         #on gèle dino pour économiser de la VRAM
         #Et que finetune dino avec si peu de données c'est un peu dangereux
-        self.backbone = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+        self.backbone = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14_reg')
         for param in self.backbone.parameters():
             param.requires_grad = False 
         
@@ -127,19 +129,22 @@ class ImageEncoder(nn.Module):
         
         self.pool = GeM()
         
+        dino_output_dim = 384
+        hidden_dim = 2048
+
         #on fait une projection patch + cls -> embed dim
         #c'est lui et les adapters qui font tout le travail
         #le rendre plus profond serait dangereux, sauf avec resblocks
         self.proj = nn.Sequential(
-            nn.Linear(768, 2048),
-            nn.LayerNorm(2048),
+            nn.Linear(dino_output_dim * 2, hidden_dim),
+            nn.LayerNorm(hidden_dim),
             nn.GELU(),
             nn.Dropout(0.3),
-            nn.Linear(2048, 1024),
-            nn.LayerNorm(1024),
+            nn.Linear(hidden_dim, hidden_dim//2),
+            nn.LayerNorm(hidden_dim//2),
             nn.GELU(),
             nn.Dropout(0.3),
-            nn.Linear(1024, 512)
+            nn.Linear(hidden_dim//2, 512)
         )
     
     def forward(self, x):
@@ -153,7 +158,6 @@ class ImageEncoder(nn.Module):
         
         embeddings = self.proj(combined)
         return F.normalize(embeddings, p=2, dim=1)
-
 
 #il mixe les deux
 class MixedEncoder(nn.Module):
